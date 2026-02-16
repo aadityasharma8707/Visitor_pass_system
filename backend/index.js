8ecb61d3a803044c811433b2e8da710bfe816dce
aaditya237001-alt <aaditya_237001@saitm.ac.in>
2026-02-16 11:58:23 +0530

    Prepare backend for deployement

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,9 @@ mongoose
   .catch((err) => console.error("Mongo error:", err.message));
 
 // 6️⃣ Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
+
